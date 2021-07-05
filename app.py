106ae16f3c31ff909dca9b6d7387fafee360a555
djsasha777 <64518378+djsasha777@users.noreply.github.com>
2021-07-05 14:42:02 +0300

    Update app.py
    ch py

--- app.py
+++ app.py
@@ -1,2 +1,20 @@
 #!usr/bin/env /python3
 print('hello from python')
+from flask import Flask, render_template
+import sqlite3
+
+if __name__ == "__main__":
+    print("Ready...")
+
+with sqlite3.connect("myhome.db") as connect:
+    kursor = connect.cursor()
+    kursor.execute("""CREATE TABLE IF NOT EXISTS smarthome (
+        temperature REAL,
+        humidity REAL,
+        pressure REAL,
+        pm10 REAL,
+        pm25 REAL,
+        pm100 REAL,
+        water1 REAL
+        )""")
+    connect.commit()
