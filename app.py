ee65ec610f197d13beaa4b60f444b7b27c2f1418
djsasha777 <djsasha777@mail.ru>
2021-07-10 20:48:27 +0300

    first server iot version

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!usr/bin/env /python3
 print('hello from python')
-from flask import Flask, render_template
+from flask import Flask, jsonify, request
 import sqlite3
 
 if __name__ == "__main__":
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 
 with sqlite3.connect("myhome.db") as connect:
     kursor = connect.cursor()
-    kursor.execute("""CREATE TABLE IF NOT EXISTS smarthome (
+    kursor.execute("""CREATE TABLE IF NOT EXISTS iot_sensors (
         temperature REAL,
         humidity REAL,
         pressure REAL,
@@ -21,9 +21,60 @@ with sqlite3.connect("myhome.db") as connect:
         digital1 INTEGER,
         digital2 INTEGER
         )""")
-    kursor.execute("""CREATE TABLE IF NOT EXISTS relays (
+    kursor.execute("""CREATE TABLE IF NOT EXISTS iot_relays (
             relay1 INTEGER,
             relay2 INTEGER,
-            mode INTEGER
+            power_mode INTEGER,
+            transfer_mode INTEGER
             )""")
     connect.commit()
+    kursor.execute("SELECT relay1 FROM iot_relays")
+    if kursor.fetchone() is None:
+        kursor.execute(f"INSERT INTO iot_relays VALUES ({0},{0},{0},{0})")
+    else:
+        pass
+    connect.commit()
+
+app = Flask(__name__)
+
+
+@app.route('/relays_data', methods=["GET"])
+def get_relays():
+    with sqlite3.connect("myhome.db") as connect:
+        kursor = connect.cursor()
+    for relays in kursor.execute("SELECT * FROM iot_relays"):
+        relays_data = {
+            "relay1": bool(relays[0]),
+            "relay2": bool(relays[1]),
+            "power_mode": bool(relays[2]),
+            "transfer_mode": bool(relays[3])
+        }
+        print(relays_data)
+    return jsonify(relays_data)
+
+
+@app.route('/post_data_from_sensors', methods=["POST"])
+def post_data():
+    sensors_details = request.get_json()
+    temperature = sensors_details["temperature"]
+    humidity = sensors_details["humidity"]
+    pressure = sensors_details["pressure"]
+    latitude = sensors_details["latitude"]
+    longitude = sensors_details["longitude"]
+    altitude = sensors_details["altitude"]
+    time = sensors_details["time"]
+    analog1 = sensors_details["analog1"]
+    analog2 = sensors_details["analog2"]
+    digital1 = sensors_details["digital1"]
+    digital2 = sensors_details["digital2"]
+    statement = "INSERT INTO iot_sensors(temperature, humidity, pressure, latitude, longitude, altitude, time, analog1, analog2, digital1, digital2) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    with sqlite3.connect("myhome.db") as connect:
+        kursor = connect.cursor()
+    kursor.execute(statement, [temperature, humidity, pressure, latitude, longitude, altitude, time, analog1, analog2, digital1, digital2])
+    connect.commit()
+    return True
+
+
+if __name__ == "__main__":
+    print("Ready to start app...")
+    app.run(host='0.0.0.0', port=80, debug=False)
