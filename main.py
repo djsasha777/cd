0a70a94f74ec4eaf8df82b7cd80508d28d962d2d
alex <x>
2021-07-26 16:47:21 +0300

    update main

--- main.py
+++ main.py
@@ -1,23 +1,59 @@
 from flask import Flask, jsonify, request
-from flask_pymongo import PyMongo
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
-mongo = PyMongo(app)
-db = mongo.db
+app.config['MONGODB_SETTINGS'] = {
+    'db':'db_name',
+    'host':'localhost',
+    'port':'27017'
+}
 
-@app.route("/")
-def home_page():
-    online_users = mongo.db.users.find({"online": True})
-    return online_users
+db = MongoEngine(app)
 
+class Sensors(db.Document):
+    hadrware = db.StringField(required=True)
+    temperature = db.FloatField()
+    humidity = db.FloatField()
+    pressure = db.FloatField()
+    latitude = db.FloatField()
+    longitude = db.FloatField()
+    altitude = db.FloatField()
+    time = db.FloatField()
+    analog1 = db.FloatField()
+    analog2 = db.FloatField()
+    digital1 = db.IntField()
+    digital2 = db.IntField()
 
-@app.route('/post_data_from_sensors', methods=["POST"])
-def post_data():
-    sensors_details = request.get_json()
-    db.sensors.insert_one(sensors_details)
+class Relays(db.Document):
+    hadrware = db.StringField(required=True)
+    relay1 = db.IntField()
+    relay2 = db.IntField()
+    power_mode = db.IntField()
+    transfer_mode = db.IntField()
+
+@app.route('/get_sensors/<id>')
+def  get_sensors(hard_id: str):
+    sensors = Sensors.objects.first_or_404(hardware=hard_id)
+    return  jsonify(sensors), 200
+
+@app.route('/get_relays/<id>')
+def  get_relays(hard_id: str):
+    relays = Relays.objects.first_or_404(hardware=hard_id)
+    return  jsonify(relays), 200
+
+@app.route('/set_relays/', methods=["PUT"])
+def update_relays(id):
+    body = request.get_json()
+    relays = Relays.objects.get_or_404(hadrware=id)
+    relays.update(**body)
     return True
 
+@app.route('/set_sensors/<id>', methods=['PUT'])
+def update_sensors(id):
+    body = request.get_json()
+    sensors = Sensors.objects.get_or_404(hadrware=id)
+    sensors.update(**body)
+    return True
 
 if __name__ == "__main__":
     print("Ready to start app...")
