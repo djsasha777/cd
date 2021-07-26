a8a841fa7b31d0846130db517f370eb4eaa2660a
alex <x>
2021-07-26 13:05:17 +0300

    add main

--- main.py
+++ main.py
@@ -1,54 +1,24 @@
-from flask import Flask, render_template, url_for, request, session, redirect
-from flask.ext.pymongo import PyMongo
-import bcrypt
+from flask import Flask, jsonify, request
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-
-app.config['MONGO_DBNAME'] = 'dbname'
-app.config['MONGO_URI'] = 'url'
-
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
 mongo = PyMongo(app)
+db = mongo.db
+
+@app.route("/")
+def home_page():
+    online_users = mongo.db.users.find({"online": True})
+    return online_users
 
 
-@app.route('/')
-def index():
-    if 'username' in session:
-        return 'You are logged in as ' + session['username']
-
-    return render_template('index.html')
-
-
-@app.route('/login', methods=['POST'])
-def login():
-    users = mongo.db.users
-    login_user = users.find_one({'name': request.form['username']})
-
-    if login_user:
-        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user[
-            'password'].encode('utf-8'):
-            session['username'] = request.form['username']
-            return redirect(url_for('index'))
-
-    return 'Invalid username/password combination'
+@app.route('/post_data_from_sensors', methods=["POST"])
+def post_data():
+    sensors_details = request.get_json()
+    db.sensors.insert_one(sensors_details)
+    return True
 
 
-@app.route('/register', methods=['POST', 'GET'])
-def register():
-    if request.method == 'POST':
-        users = mongo.db.users
-        existing_user = users.find_one({'name': request.form['username']})
-
-        if existing_user is None:
-            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
-            users.insert({'name': request.form['username'], 'password': hashpass})
-            session['username'] = request.form['username']
-            return redirect(url_for('index'))
-
-        return 'That username already exists!'
-
-    return render_template('register.html')
-
-
-if __name__ == '__main__':
-    app.secret_key = 'mysecret'
-    app.run(debug=True)
+if __name__ == "__main__":
+    print("Ready to start app...")
+    app.run(host='0.0.0.0', port=3939, debug=False)
