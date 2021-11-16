8be54792b3e686712b5a44cc171a2667d4b9b808
alex <x>
2021-11-16 12:30:27 +1000

    update

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
 app.config['MONGODB_SETTINGS'] = {
-    'db':'IOTdatabase',
+    'db':'iotdatabase',
     'host':'localhost',
-    'port':'27017'
+    'port':'27017',
+    'username':'djsasha777',
+    'password':'Fingaz3922'
 }
 
 db = MongoEngine(app)
