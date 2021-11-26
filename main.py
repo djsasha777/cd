f6f3af14e8adbbc4426cbaed19b4289d05d8f7f4
alex <x>
2021-11-26 11:23:05 +1000

    up backend

--- main.py
+++ main.py
@@ -2,18 +2,12 @@ from flask import Flask, jsonify, request
 from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
-app.config['MONGODB_SETTINGS'] = {
-    'db':'iotdatabase',
-    'host':'localhost',
-    'port':'27017',
-    'username':'djsasha777',
-    'password':'Fingaz3922'
-}
+app.config.from_pyfile('mainconfig.cfg')
 
 db = MongoEngine(app)
 
 class Sensors(db.Document):
-    hadrware = db.StringField(required=True)
+    device = db.IntField()
     temperature = db.FloatField()
     humidity = db.FloatField()
     pressure = db.FloatField()
@@ -27,35 +21,69 @@ class Sensors(db.Document):
     digital2 = db.IntField()
 
 class Relays(db.Document):
-    hadrware = db.StringField(required=True)
+    device = db.IntField()
     relay1 = db.IntField()
     relay2 = db.IntField()
     power_mode = db.IntField()
     transfer_mode = db.IntField()
 
-@app.route('/get_sensors/<id>')
-def  get_sensors(hard_id: str):
-    sensors = Sensors.objects.first_or_404(hardware=hard_id)
+@app.route('/')
+def hello_page():
+    startpage = Relays.objects()
+    return  jsonify(startpage), 200
+
+@app.route('/getsensor/<id>')
+def  get_sensors(id: str):
+    sensors = Sensors.objects.first_or_404(device=id)
     return  jsonify(sensors), 200
 
-@app.route('/get_relays/<id>')
-def  get_relays(hard_id: str):
-    relays = Relays.objects.first_or_404(hardware=hard_id)
+@app.route('/getrelay/<id>')
+def  get_relays(id: str):
+    relays = Relays.objects.first_or_404(device=id)
     return  jsonify(relays), 200
 
-@app.route('/set_relays/', methods=["PUT"])
+@app.route('/setrelay/<id>', methods=["PUT"])
 def update_relays(id):
     body = request.get_json()
-    relays = Relays.objects.get_or_404(hadrware=id)
+    relays = Relays.objects.get_or_404(device=id)
     relays.update(**body)
     return True
 
-@app.route('/set_sensors/<id>', methods=['PUT'])
+@app.route('/setsensor/<id>', methods=['PUT'])
 def update_sensors(id):
     body = request.get_json()
-    sensors = Sensors.objects.get_or_404(hadrware=id)
+    sensors = Sensors.objects.get_or_404(device=id)
     sensors.update(**body)
     return True
+
+@app.route('/addrelay/', methods=['POST'])
+def add_relays():
+    body = request.get_json()
+    relay = Relays()
+    relay.device = body.get("device")
+    relay.relay1 = body.get("relay1")
+    relay.relay2 = body.get("relay2")
+    relay.power_mode = body.get("power_mode")
+    relay.transfer_mode = body.get("transfer_mode")
+    relay.save()
+    return jsonify(relay), 201
+
+@app.route('/addsensor/', methods=['POST'])
+def add_sensors():
+    body = request.get_json()
+    sensor = Sensors()
+    sensor.device = body.get("device")
+    sensor.temperature = body.get("temperature")
+    sensor.humidity = body.get("humidity")
+    sensor.pressure = body.get("pressure")
+    sensor.latitude = body.get("latitude")
+    sensor.longitude = body.get("longitude")
+    sensor.altitude = body.get("altitude")
+    sensor.time = body.get("time")
+    sensor.analog1 = body.get("analog1")
+    sensor.analog2 = body.get("analog2")
+    sensor.digital1 = body.get("digital1")
+    sensor.digital2 = body.get("digital2")
 
 if __name__ == "__main__":
     print("Ready to start app...")
